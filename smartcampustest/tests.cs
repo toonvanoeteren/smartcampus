33c0dd072ffce8ddeeb779f07f48bb4d84a21198
toonvanoeteren <toon_vanoeteren@hotmail.com>
2017-12-01 09:15:05 +0100

    test
    test

---
+++ smartcampustest/tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace smartcampustest
+{
+    class tests
+    {
+        [TestFixture]
+        internal class TaskTests
+        {
+            [Test]
+            public void SameText()
+            {
+                string description = "Hello World";
+                string easyCompare = "Hello World";
+                Assert.That(description.Equals(easyCompare));
+            }
+        }
+    }
+}
